bb6cea3ddbb8c272c734c551b65ee93af7ddba0e
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-01 12:05:10 -0700

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t j = 0;
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		j++;
-		h = h->next;
-	}
-	return (j);
-}
+size_t j = 0;
 
+while (h != NULL)
+{
+printf("%d\n", h->n);
+j++;
+h = h->next;
+}
+return (j);
+}
