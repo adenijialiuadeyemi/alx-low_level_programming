c7418d1af3405846e0fdf944c7209b82ef860ac3
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-03 12:09:52 -0700

    Clearing bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,9 +10,11 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index < 63)
-	*n = (~(1UL << index) & *n);
-	return (1);
+if (index > 63)
 return (-1);
+
+*n = (~(1UL << index) & *n);
+return (1);
+
 }
 
