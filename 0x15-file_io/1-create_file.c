c84ba00ff5d6482f4915794d19cf49678802d48e
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-07 19:45:53 -0700

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - Getting a file created.
+ * @filename:aiming at the filename.
+ * @text_content: Aiming at the string to be written.
+ *
+ * Return:  -1 if an error occured.
+ *        but if no error occured return 1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int file_open, o, l = 0;
+
+if (!filename)
+return (-1);
+
+if (!text_content)
+{
+for (l = 0; text_content[l];)
+l++;
+}
+
+file_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+o = write(file_open, text_content, l);
+
+if (file_open == -1 || o == -1)
+return (-1);
+
+close(file_open);
+
+return (1);
+}
+
