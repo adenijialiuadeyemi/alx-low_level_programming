3e067dcde0e84b5066b8b981991e8737e98d3052
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-07 19:34:54 -0700

    reading text files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- The std output was printed.
+ * read_textfile - The std output was printed.
  * @filename: aiming at the filename to be read
  * @letters: count of letters
  * Return: w- byte number
@@ -10,20 +10,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	ssize_t file_open;
-	ssize_t o;
-	ssize_t r;
+char *buffer;
+ssize_t file_open;
+ssize_t o;
+ssize_t r;
 
-	file_open = open(filename, O_RDONLY);
-	if (file_open == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	r = read(file_open, buffer, letters);
-	o = write(STDOUT_FILENO, buf, t);
+file_open = open(filename, O_RDONLY);
+if (file_open == -1)
+return (0);
+buffer = malloc(sizeof(char) * letters);
+r = read(file_open, buffer, letters);
+o = write(STDOUT_FILENO, buffer, r);
 
-	free(buffer);
-	close(file_open);
-	return (o);
+free(buffer);
+close(file_open);
+	
+return (o);
 }
 
