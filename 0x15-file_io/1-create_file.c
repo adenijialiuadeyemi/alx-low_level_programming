f4fad373831c64602b13f1901f48cb72eadbc8e0
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-11 20:32:06 -0700

    Creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,16 +13,13 @@ int create_file(const char *filename, char *text_content)
 {
 int file_open, o, l = 0;
 
-if (filename == NULL)
+if (!filename)
 	return (-1);
-
-if (text_content!=NULL)
-{
-	for (l = 0; text_content[l];)
-		l++;
-}
-
-file_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+file_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (text_content == NULL)
+	text_content = "";
+for (l = 0; text_content[l]; l++)
+;
 o = write(file_open, text_content, l);
 
 if (file_open == -1 || o == -1)
