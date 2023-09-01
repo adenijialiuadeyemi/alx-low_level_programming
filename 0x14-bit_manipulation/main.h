0cf55fdb626557f934023df83793eb4a1199c88a
Adeniji ALiu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-31 17:35:25 -0700

    prototype for task1 was added

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* prototype for task 0 */
 unsigned int binary_to_uint(const char *b);
+
+/* prototype for task 1 */
+void print_binary(unsigned long int n);
 
 int _putchar(char c);
 #endif
