5e7a6b575f19f7baea078e3026257597f92072de
Adeniji ALiu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-31 17:54:07 -0700

    Prototype to get bit was added

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,7 @@ unsigned int binary_to_uint(const char *b);
 /* prototype for task 1 */
 void print_binary(unsigned long int n);
 
+/* prototype for getting bit */
+int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
 #endif
