7800db008ec8a785a5975e38040ed758e553ef49
Adeniji ALiu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-31 18:00:23 -0700

    updated main.h files

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,23 @@ void print_binary(unsigned long int n);
 
 /* prototype for getting bit */
 int get_bit(unsigned long int n, unsigned int index);
+
+/* prototype for setting bit */
+int set_bit(unsigned long int *n, unsigned int index);
+
+/* prototype for clearing bit */
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/* prototype for flipping bits */
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+/* prototype for _atoi */
+int _atoi(const char *s);
+
+/* prototype to get endianness */
+int get_endianness(void);
+
+/* prototype for _putchar */
 int _putchar(char c);
+
 #endif
