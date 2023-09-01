bed3f7fbc20c19285c24e2e951b3be5e1ce38f69
Adeniji ALiu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-08-31 18:10:48 -0700

    Clearing the bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - This function clears the bit value by setting it to 0
+ * @n: aiming at the number to change 
+ * @index: clear bit index
+ *
+ * Return: -1 when failed, 1 when successful
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
+
