1a20c8b80fed444458b773da92eeaf5d2cfee7d1
Adeniji ALiu Adeyemi <adenijialiuadeyemi@gmail.com>
2023-09-04 19:47:27 -0700

    updated 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	ret_write = write(fd, text_content, str_len);
 
 	if (fd == -1 || ret_write == -1)
 		return (-1);
