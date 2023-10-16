ca11c1669d2b84d6cd35884b282d0eae2b01838e
adenijialiuadeyemi <adenijialiuadeyemi@gmail.com>
2023-10-16 15:54:24 -0700

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+gcc -fPIC -c *.c
+gcc -shared *.o -o liball.so
